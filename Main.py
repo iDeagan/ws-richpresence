05ffb9a2364dd15d258f133a79f2aba6bb9dcfbe
iDeagan <63136371+iDeagan@users.noreply.github.com>
2020-06-13 09:00:59 -0700

    Forgot to update ws every 15 seconds

--- Main.py
+++ Main.py
@@ -2,10 +2,12 @@ from pypresence import Presence
 import time
 from hmisc import API_KEY
 import requests
+import random
 
-client_id = 'XXXXXXXXXXXX' #put the client_id from the application you created in here
+client_id = 'XXXXXX' #put the client_id from the application you created in here
 RPC = Presence(client_id)
 RPC.connect()
+
 
 APIKEY = API_KEY.return_api_key()
 
@@ -17,12 +19,12 @@ data = res.json()
 
 current_ws = (data["player"]["achievements"]["duels_duels_win_streak"])
 
-print(RPC.update(state="Current Win Streak: " + str(current_ws), details="Hypixel Winstreak", large_image="big-image", large_text="Made by Deagan#0001"))  # Set the presence
                                                                                             #go to discordapp.com/developers
-                                                                                            #make an application, then go to
+print("Made by Deagan#0001")                                                                                       #make an application, then go to
                                                                                             #Rich Presence > Assets and upload
                                                                                             #an image, then set the name to big-image
 while True:
+    RPC.update(state="Current Win Streak: " + str(current_ws), details="Hypixel Winstreak", large_image="big-image",large_text="Made by Deagan#0001")
     time.sleep(15)
 
     #Updates every 15 seconds
